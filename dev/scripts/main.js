732b09be71be4f573815ec65ae3f708e156fe180
Alex Tait <alexandratait@gmail.com>
2017-09-18 15:00:56 -0400

    lots of updates

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -2,10 +2,16 @@ const ohHey = "Hello World";
 
 console.log(ohHey);
 
-$(document).on('click', 'a', function(event){
-    event.preventDefault();
 
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 600);
+$(function(){
+	
+	$('.scroll').on('click', function(){
+	    $('html, body').animate({
+	        scrollTop: $( $.attr(this, 'href') ).offset().top
+	    }, 600);
+		return false;
+	});
+
+
 });
+
